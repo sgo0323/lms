eaf5a133514def770eb85f039e1570776a103408
Hajin0411 <hwalan0411@ryencatchers.com>
2023-03-08 18:27:06 +0900

    공통-버튼, 드롭박스-> 셀렉트 박스, 디자인 수정, 학습관리-학생리스트,학생리스트 데이터 없음, 스케줄관리 커밋

--- js/common.js
+++ js/common.js
@@ -14,8 +14,10 @@ function headerShortcut() {
         $(this).parents('.shortcut').find('.shortcut__content').toggleClass('shortcut__content--on');
     });
 
-    $('.shortcut').focusout(function() {
-        $(this).find('.shortcut__content').removeClass('shortcut__content--on');
+    $(document).mouseup(function (e){
+        if($(".shortcut").has(e.target).length === 0){
+            $('.shortcut__content').removeClass('shortcut__content--on');
+        }
     });
 }
 
@@ -24,18 +26,30 @@ function gnbFocus() {
     $('.gnb__item').click(function() {
         $('.gnb__item').removeClass('gnb__item--on');
         $(this).addClass('gnb__item--on');
+
+        // gnb sub 노출
+        if($(this).hasClass('gnb-sub')) {
+            $('.gnb-sub .gnb-sub__list').css('display','none'); 
+            $(this).find('.gnb-sub__list').css('display','block');
+        }
+    });
+
+    // gnb sub 비노출
+    $(document).mouseup(function (e){
+        if($('.gnb-sub').has(e.target).length === 0){
+            $('.gnb-sub__list').css('display','none');
+        }
     });
 }
 
-// input field button 활성
+// input text field button 활성
 function inputBtn() {
-    $(".input").on("keyup", function() {
+    $('.input').on('keyup', function() {
         var flag = true;
         flag = $(this).val().length > 0 ? false : true;
         $(this).parents('.input__field').find('.btn').attr('disabled', flag);
     });
 }
-
 
 $(function() {
     setTimeout(function() {
