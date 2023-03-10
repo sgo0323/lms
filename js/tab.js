8d0ea9a207f1c50190df4d909b88850305f067bc
Hajin0411 <hwalan0411@ryencatchers.com>
2023-03-10 22:47:05 +0900

    학습관리>추천학습배정, 추천학습배정 검색없음, 진도수정 커밋

--- js/tab.js
+++ js/tab.js
@@ -1,7 +1,7 @@
 function tab(){
     let tab_button = $('.tab__nav .tab__item');
 
-    tab_button.click(function(){
+    tab_button.click(function() {
         let tab_buttonEq = $(tab_button).index(this);
 
         tab_button.removeClass('tab__item--on');
@@ -9,6 +9,11 @@ function tab(){
         $('.tab__content').hide();
 
         $('.tab__content').eq(tab_buttonEq).show();
+    });
+
+    // accordion tab
+    $('.accordion__btn').click(function() {
+        $(this).parents('.accordion__item').toggleClass('accordion__item--on');
     });
 }
 
