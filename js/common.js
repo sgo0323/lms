ad9b54e67f9eefa5ad4e7c3e35c98dbc38ae5362
Hajin0411 <hwalan0411@ryencatchers.com>
2023-03-15 17:38:01 +0900

    공통 최종 디자인 적용, 기획자 디자인 수정요청 반영,  마이페이지 카테고리 pc 완료 커밋

--- js/common.js
+++ js/common.js
@@ -21,6 +21,36 @@ function headerShortcut() {
     });
 }
 
+/*
+// gnb test 
+function gnbTest() {
+    var pageUrl = window.location.href; //창의 url을 가져온다.
+    console.log('pageUrl+'+pageUrl);
+
+    $(document).on('load', function(){ //load가 되었을때 실행
+        $('.gnb .gnb__list').siblings('li').removeClass('gnb__item--on'); //다른 active가 있으면 지워준다.
+        
+        if (pageUrl.indexOf('learning') > -1) { //url에 about이라는 글자가 있으면 실행
+            console.log('learning');
+            $('.gnb__item').eq(1).addClass('gnb__item--on');            
+        } else if (pageUrl.indexOf('report') > -1) { //url에 contact라는 글자가 있으면 실행
+            console.log('report');
+            $('.gnb__list > li').eq(2).addClass('gnb__item--on');
+        } else if (pageUrl.indexOf('student') > -1) { //url에 contact라는 글자가 있으면 실행
+            console.log('student');
+            $('.gnb__list > li').eq(3).addClass('gnb__item--on');
+        } else if (pageUrl.indexOf('academy') > -1) { //url에 contact라는 글자가 있으면 실행
+            console.log('academy');
+            $('.gnb__list > li').eq(4).addClass('gnb__item--on');
+        } else {
+            console.log('pageUrl');
+            $('.gnb__list > li').eq(0).addClass('gnb__item--on'); 
+            //메인 url은 main이라는 단어가 들어가지 않아서 모든 조건이 아닐때 실행하도록 함
+        }
+    });
+}
+*/
+
 // gnb focus
 function gnbFocus() {
     $('.gnb__item').click(function() {
@@ -61,5 +91,7 @@ $(function() {
 
         // input field button 활성
        inputBtn();
+       
+       //gnbTest();
     }, 200);
 });
